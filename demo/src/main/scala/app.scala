e4ffb7829c93caf61561d587f274b1b19c831a27
lettenj61 <lettenj61@users.noreply.github.com>
2016-06-14 01:42:20 +0900

    put translation button in demo page

--- demo/src/main/scala/app.scala
+++ demo/src/main/scala/app.scala
@@ -20,7 +20,24 @@ object DemoPage extends JSApp {
   val adder = input(`type` := "button", value := "Add").render
   adder.onclick = (_: dom.Event) => fabric.rep(fromBox.value, toBox.value)
 
-  val screen = textarea(id := "trollish-app-screen", cols := 80, rows := 24, readonly := true).render
+  val screen = textarea(
+    id := "trollish-app-screen",
+    cols := 80,
+    rows := 24,
+    readonly := true
+  ).render
+
+  val translation = input(
+    id := "trollish-app-translation",
+    `type` := "text",
+    maxlength := 48
+  ).render
+  val translator = input(`type` := "button", value := "Translate").render
+  translator.onclick = (_: dom.Event) => {
+    val (eng, fiction) = fabric.showSentence(translation.value)
+    screen.value = Seq(eng, fiction, K.kanarizeAll(fiction)).mkString("\n")
+  }
+
   val randomizer = input(`type` := "button", value := "Random").render
   randomizer.onclick = (_: dom.Event) => {
     val (e, f, k) = K.randomKana
@@ -31,7 +48,9 @@ object DemoPage extends JSApp {
 
     val wrapper = dom.document.getElementById("wrapper").asInstanceOf[dom.html.Div]
     val top = div(margin := "8px")(fromBox, toBox, adder).render
-    val bottom = div(screen, hr.render, randomizer).render
+
+    val translationSection = p(margin := "8px")(translation, translator).render
+    val bottom = div(screen, hr.render, translationSection, randomizer).render
     wrapper.appendChild(
       div(top, bottom).render
     )
